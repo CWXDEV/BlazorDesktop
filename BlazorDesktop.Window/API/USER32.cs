80eca9a4e821d0d12bb74525b11275a06cfd1551
CWX <cwxdev@outlook.com>
2025-05-27 12:42:02 +0100

    add redraw call

--- BlazorDesktop.Window/API/USER32.cs
+++ BlazorDesktop.Window/API/USER32.cs
@@ -7,16 +7,16 @@ public class USER32
 {
     [DllImport("user32.dll", SetLastError = true)]
     public extern static ushort RegisterClassEx([In] ref WNDCLASSEX lpwcx);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static IntPtr CreateWindowEx(int dwExStyle, string lpClassName, string lpWindowName,
         uint dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance,
         IntPtr lpParam
     );
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static IntPtr DefWindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static bool GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
 
@@ -28,7 +28,7 @@ public class USER32
 
     [DllImport("user32.dll", SetLastError = true)]
     public extern static void PostQuitMessage(int nExitCode);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static IntPtr BeginPaint(IntPtr hWnd, out PAINT lpPaint);
 
@@ -40,25 +40,28 @@ public class USER32
 
     [DllImport("user32.dll", SetLastError = true)]
     public extern static bool SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool bRedraw);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
     [DllImport("user32.dll", SetLastError = true)]
     public extern static int GetSystemMetrics(int nIndex);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static int GetWindowLong(IntPtr hWnd, int nIndex);
-    
+
     [DllImport("user32.dll", EntryPoint = "SetClassLongPtr", SetLastError = true)]
     public extern static IntPtr SetClassLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
-    
+
     [DllImport("user32.dll", SetLastError = true)]
     public extern static bool GetClientRect(IntPtr hWnd, out RECT lpRect);
-    
+
     [DllImport("user32.dll", EntryPoint = "SetClassLongPtrW", SetLastError = true)]
     public extern static IntPtr SetClassLongPtrW(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    public extern static bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, uint flags);
 }
